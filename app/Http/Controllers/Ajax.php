2844bd17a3c243d42a08dd6b97b263b5d765933d
Putu Agus S.Kom <37550120+putuagus28@users.noreply.github.com>
2023-03-30 21:38:23 +0800

    ajax controller

---
+++ app/Http/Controllers/Ajax.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LinenType;
+use Illuminate\Http\Request;
+use App\Models\LinenCategory;
+use App\Models\Template;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class Ajax extends Controller
+{
+    public function getTemplate(Request $req)
+    {
+        // get linen template
+        $q = Template::find($req->id);
+        return response()->json($q);
+    }
+}
